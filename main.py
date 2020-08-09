faf48e45d576d50a8266fef60e805f547e6a56c4
NanoClem <clemdecoop.pro@gmail.com>
2020-08-09 04:11:36 +0200

    launch program with CLI

--- main.py
+++ main.py
@@ -1,9 +1,110 @@
 
-if __name__ == "__main__":
+#-------------------------------------------------------
+#       CHECKING PROGRAMM ARGUMENTS
+#-------------------------------------------------------
+def checkOptns(args: list) -> None:
+    """Checks if the program options are used correctly.
+    
+    Parameters
+    -----
+        args (list) -- list of program's arguments
+    
+    Raises
+    -----
+        SystemError -- Called if a wrong option is used
+    """
+    usage = f'USAGE : {args[0]} (--compression <textFile> | --decompression <binFile> <freqMap.dat>)'
+    opts  = ['--compression', '--decompression']
 
-    from HuffmanCoding import HuffmanCoding
+    if len(args) < 2:
+        raise SystemExit(f'Wrong number of arguments, {usage}')
+
+    if args[1] not in opts:
+        raise SystemError(f'Wrong program options, choose between {opts}, {usage}')
+
+
+#-------------------------------------------------------
+#       COMPRESSION
+#-------------------------------------------------------
+def execCompression(args: list) -> str:
+    """[summary]
+    
+    Parameters
+    -----
+        args (list) -- [description]
+    
+    Returns
+    -----
+        str -- [description]
+    """
+    usage = f'USAGE : {args[0]} --compression <textFile>'
+
+    # CHECKING ARGUMENTS
+    if len(args) != 3:
+        raise SystemExit(f'Wrong number of arguments, {usage}')
+    
+    if not args[2].endswith('.txt'):
+        raise SystemExit(f'Wrong file type for compression, text file expected, {usage}')
+
+    # INIT HUFFMAN COMPRESSION
+    Huff = HuffmanCoding()
 
     print("-------- STARTING COMPRESSION")
-    Huff = HuffmanCoding()
-    output = Huff.compress('alice.txt')
+    compressed = Huff.compression('alice.txt')
     print("-------- COMPRESSION COMPLETED")
+
+    return compressed
+
+
+#-------------------------------------------------------
+#       DECOMPRESSION
+#-------------------------------------------------------
+def execDecompression(args: list) -> str:
+    """[summary]
+    
+    Parameters
+    -----
+        args (list) -- [description]
+    
+    Returns
+    -----
+        str -- [description]
+    """
+    usage = f'{args[0]} --decompression <binFile> <freqMap.dat>'
+
+    # CHECKING ARGUMENTS
+    if len(args) != 4:
+        raise SystemExit(f'Wrong number of arguments, {usage}')
+
+    if not args[2].endswith('.bin'):
+        raise SystemExit(f'Wrong file type for decompression, binary file expected, {usage}')
+
+    if not args[3].endswith('.dat'):
+        raise SystemExit(f'Wrong file type for map frequencies, .dat expected, {usage}')
+
+    # INIT HUFFMAN DECOMPRESSION
+    Huff = HuffmanCoding()
+
+    print("-------- STARTING DECOMPRESSION")
+    decompressed = Huff.decompression('alice.bin')
+    print("-------- DECOMPRESSION COMPLETED")
+
+    return decompressed
+
+
+#-------------------------------------------------------
+#       MAIN
+#-------------------------------------------------------
+if __name__ == "__main__":
+
+    import sys
+    from HuffmanCoding import HuffmanCoding
+    
+    checkOptns(sys.argv)
+
+    if sys.argv[1] == '--compression':
+        execCompression(sys.argv)
+
+    ##TODO: get a freqmap file as a new argument
+    if sys.argv[1] == '--decompression':
+        execDecompression(sys.argv)
